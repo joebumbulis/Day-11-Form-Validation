e9d853f79501866c63cfd2933db1c64ef0478587
Joe Bumbulis <joebumbulis@gmail.com>
2017-03-15 21:44:21 -0500

    started styling, finished simple alerts

--- main.js
+++ main.js
@@ -9,9 +9,8 @@ var information = {};
 
 var firstName = document.querySelector('#firstName');
 var lastName = document.querySelector('#lastName');
-var button = document.querySelector('#button');
 var emailAddress = document.querySelector('emailAddress');
-var country = document.querySelector('country');
+var countrySelection = document.querySelector('country2');
 var biography = document.querySelector('biography');
 var phoneNumber = document.querySelector('phoneNumber');
 var affiliations = document.querySelector('affiliations');
@@ -19,15 +18,28 @@ var occupation = document.querySelector('occupation');
 var catName = document.querySelector('catName');
 var favgadget = document.querySelector('favgadget');
 var talent = document.querySelector('talent');
-var drink = document.querySelector('drink');
-var power = document.querySelector('power');
+var favDrink = document.querySelector('drink');
+var favPower = document.querySelector('power');
 var weapon = document.querySelector('weapon');
 var comments = document.querySelector('comments');
-
-console.dir(firstName);
+var button = document.querySelector('#button');
 
 button.addEventListener('click', function(){
 firstN();
+last();
+email();
+country();
+bio();
+phone();
+aff();
+occ();
+cat();
+gadget();
+tal();
+drink();
+power();
+weap();
+comm();
 })
 
 function firstN(){
@@ -35,6 +47,104 @@ function firstN(){
     alert('Please Fill Out First Name');
   } else {
     information.firstName = firstName.value;
+  }
+}
+function last(){
+  if (lastName.value === ''){
+    alert('Please Fill Out Last Name');
+  } else {
+    information.lastName = lastName.value;
+  }
+}
+function email(){
+  if (emailAddress.value === ''){
+    alert('Please Fill Out Email Address');
+  } else {
+    information.emailAddress = emailAddress.value;
+  }
+}
+function country(){
+  if (countrySelection.value === ''){
+    alert('Please Select a Country');
+  } else {
+    information.countrySelection = countrySelection.value;
+  }
+}
+function bio(){
+  if (biography.value === ''){
+    alert('Please Provide a bit of Biography');
+  } else {
+    information.biography = biography.value;
+  }
+}
+function phone(){
+  if (phoneNumber.value === ''){
+    alert('Please Leave your Number');
+  } else {
+    information.phoneNumber = phoneNumber.value;
+  }
+}
+function aff(){
+  if (affiliations.value === ''){
+    alert('Please Fill Out Affiliations');
+  } else {
+    information.affiliations = affiliations.value;
+  }
+}
+function occ(){
+  if (occupation.value === ''){
+    alert('Please Fill Out Occupation');
+  } else {
+    information.occupation = occupation.value;
+  }
+}
+function cat(){
+  if (catName.value === ''){
+    alert('Please Fill Out Your Cat\'s Name');
+  } else {
+    information.catName = catName.value;
+  }
+}
+function gadget(){
+  if (favgadget.value === ''){
+    alert('Please Fill Out Your Favorite Gadget');
+  } else {
+    information.favgadget = favgadget.value;
+  }
+}
+function tal(){
+  if (talent.value === ''){
+    alert('Please Fill Out Special Talents');
+  } else {
+    information.talent = talent.value;
+  }
+}
+function drink(){
+  if (favDrink.value === ''){
+    alert('Please Fill Out Favorite Drink');
+  } else {
+    information.favDrink = favDrink.value;
+  }
+}
+function power(){
+  if (favPower.value === ''){
+    alert('Please Fill Out Special Power');
+  } else {
+    information.favPower = favPower.value;
+  }
+}
+function weap(){
+  if (weapon.value === ''){
+    alert('Please Fill Your Weapon of Choice');
+  } else {
+    information.weapon = weapon.value;
+  }
+}
+function comm(){
+  if (comments.value === ''){
+    alert('Please leave a Comment');
+  } else {
+    information.comments = comments.value;
   }
 }
 
---
+++ talents.js
@@ -0,0 +1,10 @@
+var talents_arr = new Array('Beat-Box', 'Humming', 'Singing', 'Whistling', 'Flexible', 'Photographic Memory');
+
+// function populateTalents(countryElementId, stateElementId){
+// 	// given the id of the <select> tag as function argument, it inserts <option> tags
+// 	var countryElement = document.getElementById(countryElementId);
+// 	countryElement.length=0;
+// 	countryElement.options[0] = new Option('Select Country','-1');
+// 	countryElement.selectedIndex = 0;
+// 	for (var i=0; i<country_arr.length; i++) {
+// 		countryElement.options[countryElement.length] = new Option(country_arr[i],country_arr[i]);
