339bfc66f8b6a81575c7371d2d4b7419016e2e14
Joe Bumbulis <joebumbulis@gmail.com>
2017-03-15 20:46:42 -0500

    full form html

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 * If their input is invalid, you must somehow inform the user that they need to fill out more information. You can be very generic in this message.
 * If their input is valid, put the input in an object and print that object to the console.
 
-##  Notes
+####  Notes
 Don't worry about styles until you've completed Explorer mode! I recommend starting by just making a form with only one input, and validating that one input. Once you've got that working, you can add another element and validate that new one.
--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-
+console.log('Hello World')
+//when the user clicks the submit button- runs a function to test to so if their input is validates
+//if invalid - alert with generic 'fill out ore info'
+//else - return the info into an object
+//console.log the object
 
 
 var information = {};
 
-var inputs = getElementsByClassName('input');
-var button = getElementsByClassName('button');
+var firstName = document.querySelector('#firstName');
+var lastName = document.querySelector('#lastName');
+var button = document.querySelector('#button');
+var emailAddress = document.querySelector('emailAddress');
+var country = document.querySelector('country');
+var biography = document.querySelector('biography');
+var phoneNumber = document.querySelector('phoneNumber');
+var affiliations = document.querySelector('affiliations');
+var occupation = document.querySelector('occupation');
+var catName = document.querySelector('catName');
+var favgadget = document.querySelector('favgadget');
+var talent = document.querySelector('talent');
+var drink = document.querySelector('drink');
+var power = document.querySelector('power');
+var weapon = document.querySelector('weapon');
+var comments = document.querySelector('comments');
 
-button.addEventListener('click', function(evt){
+console.dir(firstName);
 
+button.addEventListener('click', function(){
+firstN();
 })
+
+function firstN(){
+  if (firstName.value === ''){
+    alert('Please Fill Out First Name');
+  } else {
+    information.firstName = firstName.value;
+  }
+}
+
+console.log(information)
